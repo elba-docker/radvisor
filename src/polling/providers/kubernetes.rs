1b28150d51a0a99cff6fa1a6df4a09a37878665f
Joseph Azevedo <joseph.az@gatech.edu>
2020-03-27 05:52:42 -0400

    fix: update docstring; fix formatting

--- src/polling/providers/kubernetes.rs
+++ src/polling/providers/kubernetes.rs
@@ -49,7 +49,7 @@ impl Provider for Kubernetes {
         // Initialize client
         self.client = Some(APIClient::new(config));
 
-        // Get current node by hostname
+        // Get current node by hostname and store in provider
         self.node_name =
             match self.get_node_by_hostname(&hostname) {
                 Some(node) => Some(Meta::meta(&node).name.as_ref().unwrap().clone()),
@@ -57,20 +57,26 @@ impl Provider for Kubernetes {
                     "Could not get the current node via the Kubernetes API.\nMake sure the current machine is running its own node.",
                 )),
             };
-
-        println!("Current node: {}", self.node_name.as_ref().unwrap());
-
-        Some(ConnectionError::new("found node successfully"))
+        None
     }
 
     fn fetch(&mut self) -> Result<Vec<ContainerMetadata>, FetchError> {
+        // TODO implement
+        // 1. get list of pods where they are on the current node (by nodeName)
+        // 2. get all IDs from pod metadata, build 3 paths to look at for them
+        // 3. look at filesystem for the cgroup slices, see if 1/3 of them exist
+        //    - cgroup slice: kubepods-{1}.slice/kubepods-{1}-pod{2}.slice
+        //      where 1 in ["besteffort", "burstable", "guaranteed"]
+        //            2 = pod.ID.replace("-", "_")
+        // 4. if the slice exists by using IO call, write metadata info to string,
+        //    store existing cgroup, pass to collection thread
         Ok(Vec::with_capacity(0))
     }
 }
 
 impl Kubernetes {
-    /// Executes a future on the single-threaded runtime, blocking the current thread
-    /// until it completes
+    /// Executes a future on the internal runtime, blocking the current thread until
+    /// it completes
     fn exec<F: Future>(&self, future: F) -> F::Output {
         let mut runtime = self.runtime.borrow_mut();
         runtime.block_on(future)
